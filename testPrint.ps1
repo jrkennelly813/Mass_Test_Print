6d9d7a95df1d4c78d1fcd7983b523851f5be1fac
Justin Robert Kennelly <jrkennelly813@gmail.com>
2022-04-19 08:21:03 -0700

    Create testPrint.ps1

---
+++ testPrint.ps1
@@ -0,0 +1,16 @@
+﻿$port = Read-Host "Enter the 2-Letter Prefix for the site "
+Get-Printer | Where-Object {$_.Shared -eq $true -and $_.PortName -Like "$($port)-*"}
+    ForEach-Object {
+        $printerName = $_.Name
+        $result = Get-CimInstance Win32_Printer -Filter "name LIKE '$printerName'" |
+            Invoke-CimMethod -MethodName printtestpage 
+        if ($result.ReturnValue -eq 0)
+        {
+            "Test page printed on $printerName."
+        }
+        else
+        {
+            "Unable to print test page on $printerName."
+            "Error code $($result.ReturnValue)."
+        }
+    }
